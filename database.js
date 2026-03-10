e88ec94461b6c3418a75cc9978662e1adefe161e
Physgen02 <michael.altfield@yahoo.com>
2026-03-09 23:13:01 -0700

    Create database.js

---
+++ database.js
@@ -0,0 +1,23 @@
+const league = {
+
+teams:[
+{name:"Trash Pandas",wins:0,losses:0,points:0},
+{name:"Ice Breakers",wins:0,losses:0,points:0},
+{name:"Blue Blades",wins:0,losses:0,points:0},
+{name:"Arctic Wolves",wins:0,losses:0,points:0}
+],
+
+schedule:[
+{date:"May 1",home:"Trash Pandas",away:"Ice Breakers"},
+{date:"May 1",home:"Blue Blades",away:"Arctic Wolves"}
+],
+
+players:[
+{name:"Player 1",team:"Trash Pandas",gp:0,g:0,a:0,p:0,pim:0}
+],
+
+goalies:[
+{name:"Goalie 1",team:"Trash Pandas",minutes:0,shots:0,saves:0,ga:0}
+]
+
+}
